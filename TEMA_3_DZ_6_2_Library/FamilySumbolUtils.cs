a340ae71869ca7523f6defb4987a867a149b6e93
Марис <122129172+Maris-ITMO@users.noreply.github.com>
2023-05-10 13:21:30 +0300

    Add project files.

---
+++ TEMA_3_DZ_6_2_Library/FamilySumbolUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace TEMA_3_DZ_6_2_Library
+{
+    public class FamilySumbolUtils
+    {
+
+        public static List<FamilySymbol> GetFurnitureSymbols(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var furnitureTypes = new FilteredElementCollector(doc)
+                .WherePasses(new ElementCategoryFilter(BuiltInCategory.OST_Furniture))
+                .OfClass(typeof(FamilySymbol))
+                .Cast<FamilySymbol>()
+                .ToList();
+
+            return furnitureTypes;
+        }
+    }
+}
---
+++ TEMA_3_DZ_6_2_Library/LevelsUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace TEMA_3_DZ_6_2_Library
+{
+    public class LevelsUtils
+    {
+        public static List<Level> GetLevels(ExternalCommandData commandData)
+        {
+            var doc = commandData.Application.ActiveUIDocument.Document;
+            List<Level> levels = new FilteredElementCollector(doc)
+                .OfClass(typeof(Level))
+                .Cast<Level>()
+                .ToList();
+
+            return levels;
+        }
+    }
+}
